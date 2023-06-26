fcb879cb1e8f2fe58fc252d155399aaf82fbaf08
RikardoFlamenguista <lhss2003@gmail.com>
2023-06-25 21:57:36 -0300

    atualizações level 2

--- Assets/Scenes/Scripts/Inimigos Scripts/InimigoMissil.cs
+++ Assets/Scenes/Scripts/Inimigos Scripts/InimigoMissil.cs
@@ -16,8 +16,7 @@ public class InimigoMissil : MonoBehaviour
     public AudioSource somMorte; // Som que sera executado
     public WaveManager waveManager;// script que faz parte do WaveManager
     public GameObject MorteFx;
-    public delegate void EnemyKilled();
-    public static event EnemyKilled OnEnemyKilled;
+
 
     public GameObject particlePrefab;
     public CinemachineImpulseSource source;
@@ -45,6 +44,10 @@ public class InimigoMissil : MonoBehaviour
     private float chanceItemVida;
     [SerializeField] private ItemDeCura itemvidaprefab;
 
+    [Header("Configurações de distância entre inimigos")]
+    public float distMinimaEntreInimigos = 2f; // Distância mínima desejada entre os inimigos
+    public float desvioForca = 1f; // Força do desvio aplicado para evitar colisões
+
     // Start is called before the first frame update
     void Start()
     {
@@ -60,19 +63,77 @@ public class InimigoMissil : MonoBehaviour
         // Verifica se o objeto do jogador foi definido
         if (Player == null) return;
 
-        // C�lculo da dire��o para o jogador
-        Vector3 direction = Player.position - transform.position;
-        direction.Normalize();
+        // Cálculo da direção para o jogador
+        Vector3 playerDirection = Player.position - transform.position;
+        playerDirection.Normalize();
 
-        // C�lculo do �ngulo de rota��o em rela��o ao jogador
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
-        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        // Calcula a distância atual entre o inimigo e o jogador
+        float currentDistance = Vector3.Distance(transform.position, Player.position);
 
-        // Aplica a rota��o e movimento
-        transform.rotation = rotation;
-        transform.position += direction * moveSpeed * Time.deltaTime;
+        // Verifica se a distância atual é menor que a distância mínima desejada
+        if (currentDistance < distMinimaEntreInimigos)
+        {
+            // Calcula a direção oposta à direção para o jogador
+            Vector3 avoidanceDirection = -playerDirection;
+
+            // Suaviza a direção de desvio usando um algoritmo de interpolação
+            Vector3 smoothDirection = Vector3.Lerp(transform.up, avoidanceDirection, Time.deltaTime * 5f);
+
+            // Cálculo do ângulo de rotação em relação à direção suavizada
+            float angle = Mathf.Atan2(smoothDirection.y, smoothDirection.x) * Mathf.Rad2Deg - 90f;
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            // Aplica a rotação e movimento
+            transform.rotation = rotation;
+            transform.position += smoothDirection * moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            // Aplica o desvio suave para evitar colisões com outros inimigos
+            Vector3 avoidanceDirection = AvoidOtherEnemies();
+            Vector3 desiredDirection = playerDirection + avoidanceDirection;
+
+            // Suaviza a direção desejada usando um algoritmo de interpolação
+            Vector3 smoothDirection = Vector3.Lerp(transform.up, desiredDirection, Time.deltaTime * 5f);
+
+            // Cálculo do ângulo de rotação em relação à direção suavizada
+            float angle = Mathf.Atan2(smoothDirection.y, smoothDirection.x) * Mathf.Rad2Deg - 90f;
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            // Aplica a rotação e movimento
+            transform.rotation = rotation;
+            transform.position += smoothDirection * moveSpeed * Time.deltaTime;
+        }
     }
+    private Vector3 AvoidOtherEnemies()
+    {
+        Vector3 avoidanceDirection = Vector3.zero;
+        int enemyCount = 0;
 
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Taginimigo");
+        foreach (GameObject enemy in enemies)
+        {
+            if (enemy != null && enemy != this.gameObject)
+            {
+                float distance = Vector3.Distance(transform.position, enemy.transform.position);
+                if (distance < distMinimaEntreInimigos)
+                {
+                    Vector3 avoidance = transform.position - enemy.transform.position;
+                    avoidanceDirection += avoidance.normalized / distance; // Peso inversamente proporcional à distância
+                    enemyCount++;
+                }
+            }
+        }
+
+        if (enemyCount > 0)
+        {
+            avoidanceDirection /= enemyCount;
+            avoidanceDirection.Normalize();
+            avoidanceDirection *= desvioForca;
+        }
+
+        return avoidanceDirection;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "tiroPlayer")
@@ -95,17 +156,13 @@ public class InimigoMissil : MonoBehaviour
                 Destroy(this.gameObject);
                 GameManager.instancia.adicionarPontos(recompensaPontos);
                 AudioManager.instancia.TocarSomMorte();
-                AudioManager.instancia.GetComponent<AudioSource>().PlayOneShot(AudioManager.instancia.explosaoSFX, 0.5f); 
+                AudioManager.instancia.GetComponent<AudioSource>().PlayOneShot(AudioManager.instancia.explosaoSFX, 0.5f);
 
 
             }
 
 
-            if (other.transform.tag == "Player")
-            {
-                GameManager.instancia.vidaAtual = 0;
 
-            }
 
 
         }
@@ -153,13 +210,6 @@ public class InimigoMissil : MonoBehaviour
         }
 
     }
-
-    void OnDestroy()
-    {
-        if (OnEnemyKilled != null)
-        {
-            OnEnemyKilled();
-        }
-    }
-
 }
+
+   
